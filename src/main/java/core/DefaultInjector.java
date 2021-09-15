823f542b5ab336b17b748160f2acd1acbd918032
s.dvaradkin <s.dvaradkin@godeltech.com>
2021-09-15 19:04:32 +0300

    remove unused fields

--- src/main/java/core/DefaultInjector.java
+++ src/main/java/core/DefaultInjector.java
@@ -6,7 +6,6 @@ import java.lang.reflect.Constructor;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 public class DefaultInjector implements Injector {
     private final Map<Class<?>, Binding<?>> bindings = new HashMap<>();
@@ -30,8 +29,7 @@ public class DefaultInjector implements Injector {
 
     private <T> void doBind(Class<T> type, Class<? extends T> impl, boolean isSingleton) {
         Constructor<? extends T> implConstructor = findConstructorForInjection(impl);
-        Class<?>[] typeDependencies = implConstructor.getParameterTypes();
-        bindings.put(type, new Binding<>(type, typeDependencies, implConstructor, isSingleton, bindings));
+        bindings.put(type, new Binding<>(implConstructor, isSingleton, bindings));
     }
 
     private <T> Constructor<? extends T> findConstructorForInjection(Class<? extends T> impl) {

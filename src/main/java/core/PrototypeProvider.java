f26166de2d4e4d02839dd6f0202083d5cac88b33
s.dvaradkin <s.dvaradkin@godeltech.com>
2021-09-15 16:55:07 +0300

    Add PrototypeProvider

---
+++ src/main/java/core/PrototypeProvider.java
@@ -0,0 +1,26 @@
+package core;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+public class PrototypeProvider<T> implements Provider<T> {
+    private final Constructor<T> constructor;
+    private final List<Provider<?>> dependencyProviders;
+
+    public PrototypeProvider(Constructor<T> constructor, List<Provider<?>> dependencyProviders) {
+        this.constructor = constructor;
+        this.dependencyProviders = dependencyProviders;
+    }
+
+    @Override
+    public T getInstance() {
+        Object[] dependencies = dependencyProviders.stream().map(Provider::getInstance).toArray();
+        try {
+            return constructor.newInstance(dependencies);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            // TODO throw proper exception
+            throw new RuntimeException(e);
+        }
+    }
+}
